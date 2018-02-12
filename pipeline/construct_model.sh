a1451633638fa19e1da2227ad0aa5cf5064eb44e
mengzhou <mengzhou@usc.edu>
2018-02-12 14:42:16 -0800

    Added pipeline scripts for constructing and training model.

---
+++ pipeline/construct_model.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+# Input: BED file converted from nhmmer scan result
+# Needs to add parameter support of TRF pipeline
+REF=/home/rcf-40/mengzhou/panfs/repeats/ms1509/ms1509.fa
+TRF_PROG=/home/rcf-40/mengzhou/panfs/repeats/cyclic/sim/trf407b.linux64
+REV_COMP=/home/rcf-40/mengzhou/scripts/utils/revcomp_fa.py
+MUSCLE=/home/rcf-40/mengzhou/panfs/tools/muscle3.8.31_i86linux64
+RPHMM=/home/rcf-40/mengzhou/panfs/repeats/RepeatProfileHMM/bin
+GET_MEDIAN=/home/rcf-40/mengzhou/scripts/computing/get_median.awk
+NTHREAD=16
+
+# file names involved in this program
+NHMMER_OUT=""
+INIT_PROMOTER=""
+
+function muscle_align() {
+  $MUSCLE -in $1 -quiet -clw > ${1%.*}.aln
+}
+
+function trf_by_revcomp() {
+  INPUT_FA=$1
+  EXPECTED_START=$2
+  EXTENSION=$3
+  NAME=${INPUT_FA%.fa}
+  TRF_PARS="2 5 5 80 10 50 500 -h -ngs"
+  MONOMER_MIN_LEN=180
+  MONOMER_MAX_LEN=220
+  MONOMER_MIN_CN=2.0
+  MONOMER_MIN_START=$(expr $EXPECTED_START - $EXTENSION)
+  MONOMER_MAX_START=$(expr $EXPECTED_START + $EXTENSION)
+  INPUT_REV=${NAME}.rev.fa
+  $REV_COMP $INPUT_FA > $INPUT_REV
+  $TRF_PROG $INPUT_REV $TRF_PARS > ${NAME}.rev.monomers.list
+  cat ${NAME}.rev.monomers.list | awk 'BEGIN{OFS="\t"; name="";}\
+    {if($1~/^@/){name=substr($1, 2);}else{print name, $1, $5, $4, $14}}' | \
+    sort -k3,3n -k1,1 > ${NAME}.rev.monomers.data
+  awk "BEGIN{flag=0;l=0}{if(substr(\$0,1,1)==\"@\")\
+      {l=\$0; flag=1}else if(flag==1 && \$5>=$MONOMER_MIN_LEN && \
+      \$5<=$MONOMER_MAX_LEN && \$4>=$MONOMER_MIN_CN\
+      && \$1>=$MONOMER_MIN_START && \$1<=$MONOMER_MAX_START){print l; \
+      print \$14; flag=0}}" ${NAME}.rev.monomers.list | \
+    sed 's/^@/>/g' > ${NAME}.rev.monomers.fa
+  $REV_COMP ${NAME}.rev.monomers.fa > ${NAME}.monomers.fa
+  rm ${NAME}.rev.monomers.fa $INPUT_REV
+}
+
+function get_init_promoter() {
+  # get potential promoter regions by merging all monomers and extend by
+  # PROMOTER_EXT bp. The promoter regions are stranded for now to allow TRF.
+  echo "[GENERATING EXTENDED PROMOTERS]"
+  IN=$1
+  NAME=${IN%.bed}
+  NHMMER_EXT=$2
+  PROMOTER_EXT=$3
+  INIT_PROMOTER=${FAMILY}.ext${PROMOTER_EXT}
+  cat $IN | mergeBed -d $NHMMER_EXT -s | sort -k1,1 -k2,2n -k3,3n | \
+    awk -v ext=$PROMOTER_EXT -v family=$NAME \
+    'BEGIN{OFS="\t"}{start=$2-ext; end=$3+ext; strand=$4;\
+      print $1, start, end, family"|"$1":"start"-"end"|"strand, end-start, strand;\
+    }' > ${INIT_PROMOTER}.bed
+  bedtools getfasta -s -name -fi $REF -bed ${INIT_PROMOTER}.bed -fo \
+    ${INIT_PROMOTER}.fa
+}
+
+function get_init_monomer() {
+  # get monomers using TRF, and construct the initial model
+  echo "[CONSTRUCTING INITIAL MODEL]"
+  IN=$1
+  trf_by_revcomp $IN 200 50
+  muscle_align ${1%.*}.monomers.fa
+  $RPHMM/construct -o ${1%.*}.initial.params ${1%.*}.monomers.aln
+}
+
+function scan_rpmm() {
+  IN_FA=$1
+  MODEL=$2
+  OUT_PREFIX=$3
+  echo "[SCANNING]"
+  $RPHMM/scan -v -p $NTHREAD -c $IN_FA -o ${OUT_PREFIX}.scan $MODEL
+  cat ${OUT_PREFIX}.scan | awk 'BEGIN{OFS="\t"}{split($1, f, "|"); \
+      split(f[2], ff, "[:-]"); strand=f[3]; if(strand=="+"){start=ff[2]+$2; \
+      end=ff[2]+$3}else{start=ff[3]-$3; end=ff[3]-$2;} \
+      print ff[1], start, end, $1, end-start, strand, $NF }' | \
+    sort -k1,1 -k2,2n -k3,3n -k6,6 > ${OUT_PREFIX}.bed
+}
+
+function get_top_n_median() {
+  NUM=$1
+  IN_BED=$2
+  CORE_LEN=$(cut -f 5 $IN_BED | sort | uniq -c | \
+    awk 'BEGIN{OFS="\t"}{print $2, $1}' | sort -k2nr | head -n $NUM | \
+    cut -f 1 | sort -k1n | awk -f $GET_MEDIAN)
+}
+
+function sample_by_len() {
+  IN_BED=$1
+  LEN=$2
+  EXT=$3
+  NUM=$4
+  cat $IN_BED | awk -v l=$LEN -v ext=$EXT \
+    'BEGIN{OFS="\t";srand()}{if($5>=l-ext && $5<=l+ext){print rand(), $0}}' | \
+    sort -k1,1g | head -n $NUM | cut -f 2- | sort -k1,1 -k2,2n -k3,3n > \
+    ${IN_BED%.*}.rand${NUM}.bed
+  bedtools getfasta -s -name -fi $REF -bed ${IN_BED%.*}.rand${NUM}.bed \
+    -fo ${IN_BED%.*}.rand${NUM}.fa
+  sed -i 's/[nN]/T/g' ${IN_BED%.*}.rand${NUM}.fa
+}
+
+if [ $# -lt 1 ]
+then
+  echo "Usage: $0 <nhmmer output .bed>"
+  exit 0
+fi
+
+FAMILY=${1%.*}
+get_init_promoter $1 20 200
+get_init_monomer ${INIT_PROMOTER}.fa
+# first scan using the initial model to determine monomer bounds
+scan_rpmm ${INIT_PROMOTER}.fa ${INIT_PROMOTER}.initial.params ${INIT_PROMOTER}.split
+# estimate the model length by getting the median of top 8 abundant lengths
+get_top_n_median 8 ${INIT_PROMOTER}.split.bed
+# Randomly sample monomers that are close to the core length to build
+# a refined model
+echo "Model lenths is estimated to be ${CORE_LEN}."
+sample_by_len ${INIT_PROMOTER}.split.bed $CORE_LEN 20 500
+muscle_align ${INIT_PROMOTER}.split.rand${NUM}.fa
+
+$RPHMM/construct -u -o ${INIT_PROMOTER}.split.rand${NUM}.params \
+  ${INIT_PROMOTER}.split.rand${NUM}.aln
+scan_rpmm ${INIT_PROMOTER}.fa ${INIT_PROMOTER}.split.rand${NUM}.params \
+  ${INIT_PROMOTER}.split.rand${NUM}
---
+++ pipeline/iteration_rphmm.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+# TO-DO: deambiguity_GT.bed needs to be changed to something more general
+NHMMER=~mengzhou/panfs/repeats/mm10/RepeatMasker/L1Base/scan/model_construction/hmmer/nhmmer
+MUSCLE=~mengzhou/panfs/tools/muscle3.8.31_i86linux64
+ESL=~mengzhou/panfs/tools/hmmer-3.1b1/binaries/esl-reformat
+BUILD=~mengzhou/panfs/tools/hmmer-3.1b1/binaries/hmmbuild
+RPHMM=/home/rcf-40/mengzhou/panfs/repeats/RepeatProfileHMM/bin
+NTHREAD=16
+
+GENOME=/home/rcf-40/mengzhou/panfs/repeats/ms1509/ms1509.fa
+RAND=500
+MAX_ITR=9
+JACCARD=0
+
+function float_lt () {
+  echo $1"<"$2 | bc -l
+}
+function prep_seq() {
+  # $1: consensus.fa
+  # $2: nhmmer_scan.bed of previous iteration
+  # $3: name of the output, usually the name of current iteration
+  # $4: number of sequences to sample
+  # $5: length of the consensus
+  echo "[EXTRACTING SEQ]"
+  REG=${3}.bed
+  echo $1, $2, $REG, $4, $5
+  TEMP=$(mktemp tmp.$$.XXXXXXXX)
+  # the score is length normalized -log(E-value)
+  cat $2 | awk -v LEN=$5 -v EXT=15 'BEGIN{OFS="\t"}{\
+      score=-log($8)/$5; if($5<=LEN+EXT && $5>=LEN-EXT)\
+      {print $1, $2, $3, $4"|"$1":"$2"-"$3"|"$6, score, $6}}' | sort -k5,5gr > $TEMP
+  TOTAL=$(cat $TEMP | wc -l)
+  echo $TOTAL
+  # keep the first 1/3 scoring hits for random sampling
+  cat $TEMP | sort -k5,5gr | awk -v t=$TOTAL 'BEGIN{OFS="\t"; srand()}NR<=t/1.5\
+      {print rand(), $0}' | sort -k1,1g | cut -f 2- | awk -v s=$4 'NR<=s' | \
+    sort -k1,1 -k2,2n -k3,3n > $REG
+  rm $TEMP
+  bedtools getfasta -fi $GENOME -s -name -bed $REG -fo ${3}.fa
+  cat $1 >> ${3}.fa
+}
+
+function muscle() {
+  echo "[ALIGNING]"
+  $MUSCLE -in $1 -clw -quiet > ${1%.*}.aln
+  $ESL stockholm ${1%.*}.aln | sed 's/consensus/#=GC RF/' > ${1%.*}.sto
+}
+
+function build_nhmmer() {
+  $BUILD --hand --dna --plaplace --wnone --enone ${1%.*}.hmm $1
+}
+
+function build_rphmm() {
+  $RPHMM/construct -m -o ${1%.*}.params $1
+}
+
+function scan_nhmmer() {
+  # $1: profile-HMM
+  # $2: reference genome FASTA
+  # $3: output BED name
+  # $4: (optional) filtering region for removing ambiguity in F type monomer detection
+  $NHMMER -o /dev/null --cpu 16 --dna --dfamtblout ${3%.*}.out --max --cut_ga $1 $2
+  awk 'BEGIN{OFS="\t"}$1!~/#/{if($9=="+"){start=$10-1; end=$11;}\
+      else{start=$11-1; end=$10} len=end-start; score=-log($5)/len;\
+      if(score >= .2)print $1, start, end, $3, end-start, $9, $4, $5}' ${3%.*}.out | \
+    sort -k1,1 -k2,2n -k3,3n > $3
+  if [ $4 ]
+  then
+    mv $3 ${3}.all
+    intersectBed -a ${3}.all -b $4 -v > $3
+  fi
+}
+
+function scan_rphmm() {
+  # $1: profile-HMM
+  # $2: reference genome FA
+  # $3: output BED
+  $RPHMM/scan -v -p $NTHREAD -c $2 $1 | \
+    sort -k1,1 -k2,2n -k3,3n > ${3%.*}.out
+  awk 'BEGIN{OFS="\t"}{split($1, f, "|"); split(f[2], ff, "[:-]");\
+      chr=ff[1]; start=ff[2]; end=ff[3];\
+      if(length(f)==2){strand=$6; print chr, start+$2, start+$3, $1, $3-$2, strand, $7}\
+      else{strand=f[3];if(strand=="+"){print chr, start+$2, start+$3, $1, $3-$2, strand, $7}\
+      else{print chr, end-$3, end-$2, $1, $3-$2, strand, $7}}}' ${3%.*}.out | \
+    sort -k1,1 -k2,2n -k3,3n > $3
+}
+
+if [ $# -lt 3 ]
+then
+  echo "Usage: $0 <CONSENSUS.FA> <HMMER_OUTPUT_BED> <SEQ_OF_ROI>"
+  exit
+else
+  export -f scan_rphmm
+  LEN=$(tail -n +2 $1 | awk '{print length($1)}')
+
+  PREV_OUT=$2
+  REF=$3
+  ITR=1
+  CONVERGE=$(float_lt $JACCARD 0.99)
+  while [ \( $ITR -le $MAX_ITR \) -a $CONVERGE -eq 1 ]
+  do
+    echo "[ITERATION $ITR]"
+    NAME=${PREV_OUT%%.*}.${ITR}.20bp.rand${RAND}
+    CURR_OUT=${NAME}.scan.bed
+    prep_seq $1 $PREV_OUT $NAME $RAND $LEN
+    muscle ${NAME}.fa
+
+    #nhmmer
+    #build_nhmmer ${NAME}.sto
+    #echo "[SCANNING]"
+    #scan_nhmmer ${NAME}.hmm $GENOME $CURR_OUT
+
+    #rphmm
+    echo "[BUILDING]"
+    build_rphmm ${NAME}.aln
+    echo "[SCANNING]"
+    #for i in {0..7};do echo $i; done | parallel \
+    #  "scan_rphmm ${NAME}.params list.{}.fa ${NAME}.{}.scan.bed"
+    #cat ${NAME}.?.scan.out | sort -k1,1 -k2,2n -k3,3n > ${CURR_OUT%.*}.out
+    #cat ${NAME}.?.scan.bed | sort -k1,1 -k2,2n -k3,3n > $CURR_OUT
+    scan_rphmm ${NAME}.params $REF $CURR_OUT
+
+    INTER=$(intersectBed -a $PREV_OUT -b $CURR_OUT | sort -k1,1 -k2,2n -k3,3n | mergeBed\
+      | awk 'BEGIN{t=0}{t+=$3-$2}END{print t}')
+    UNION=$(cat $PREV_OUT $CURR_OUT | cut -f 1-3 | sort -k1,1 -k2,2n -k3,3n | \
+      mergeBed | awk 'BEGIN{t=0}{t+=$3-$2}END{print t}')
+    JACCARD=$(echo "$INTER / $UNION" | bc -l)
+    echo "[JACCARD=$JACCARD]"
+    CONVERGE=$(float_lt $JACCARD 0.95)
+    let ITR=ITR+1
+    PREV_OUT=$CURR_OUT
+    echo ""
+  done
+fi
