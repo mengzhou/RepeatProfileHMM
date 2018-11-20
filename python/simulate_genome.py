77218cd2e56c2da4260cf30c01017881716511de
mengzhou <mengzhou@usc.edu>
2018-11-19 17:36:40 -0800

    Changed test data.

--- python/simulate_genome.py
+++ python/simulate_genome.py
@@ -30,7 +30,7 @@ def opt_validation(parser, opt):
     sys.exit(0)
 
   family_count = len(opt.repeat)
-  for par in ["number", "trunc_par", "trunc_lower", "age"]:
+  for par in ["number", "trunc_par", "sub_par", "indel_par", "age"]:
     if len(getattr(opt,par)) < family_count:
       setattr(opt, par, getattr(opt, par) * family_count)
 
@@ -51,15 +51,17 @@ def main():
   parser.add_option("-s", "--output-repeats", action="store", type="string",
     dest="out_repeats", help="Output path for sequence of inserted repeats.")
   parser.add_option("-n", "--num", action="callback", type="string",
-    default=[10], dest="number", help="Number of insertions. Default: 10.",\
+    default=[50], dest="number", help="Number of insertions. Default: 50.",\
     callback=parse_int_list)
   parser.add_option("-t", "--truncation", action="callback", type="string",
     dest="trunc_par", help="Parameter for truncation probability. "+\
-    "Defalut: 0.005", default=[0.005], callback=parse_float_list)
-  parser.add_option("-l", "--lower", action="callback", type="string",
-    default=[10], dest="trunc_lower", \
-    help="Lower bound of length left after truncation. Default: 10.",\
-    callback=parse_int_list)
+    "Defalut: 0.35", default=[0.35], callback=parse_float_list)
+  parser.add_option("-m", "--substitution", action="callback", type="float",
+    dest="sub_par", help="Parameter for substitution probability. "+\
+    "Defalut: 0.02", default=[0.02], callback=parse_float_list)
+  parser.add_option("-i", "--indel", action="callback", type="float",
+    dest="indel_par", help="Parameter for indel probability. "+\
+    "Defalut: 0.0008", default=[0.0008], callback=parse_float_list)
   parser.add_option("-e", "--age", action="callback", type="string",
     dest="age", help="Parameter for family age (in Mya). "+\
     "Defalut: 10.0", default=[10.0], callback=parse_float_list)
@@ -72,15 +74,16 @@ def main():
   consensus_name = []
   consensus_seq = []
   families = []
+  mutators = [mutations(opt.sub_par[i], opt.indel_par[i], opt.trunc_par[i]) \
+      for i in range(len(opt.repeat))]
 
   for idx, infile in enumerate(opt.repeat):
     name, seq = load_fasta(infile)
     consensus_name.append(name)
     consensus_seq.append(seq)
-    new_family = repeat_family(seq, name, opt.trunc_par[idx], opt.trunc_lower[idx])
-    new_family.generate(opt.number[idx], opt.age[idx])
-    # generate full length copies for model training
-    #new_family.generate_full_length(100, opt.age[idx])
+    new_family = repeat_family(name, seq, opt.age[idx], opt.number[idx], \
+        mutators[idx])
+    new_family.generate_copies(True, False)
     families.append(new_family)
 
   insertion_sites = generate_insertion_sites(len(genome_seq), sum(opt.number))
@@ -90,9 +93,10 @@ def main():
   all_copy_mut_sites = []
   for i in families:
     for j in i.copies:
-      all_copy_seq.append(j["seq"])
-      all_copy_name.append(j["name"])
-      all_copy_mut_sites.append(j["mut_sites"])
+      r = i.copies[j]
+      all_copy_seq.append(r.actual_seq)
+      all_copy_name.append(r.name+"_"+r.label)
+      all_copy_mut_sites.append(len(r.subs)+len(r.indels))
 
   shuffle_order = range(len(all_copy_seq))
   r = random.shuffle(shuffle_order)
@@ -120,14 +124,13 @@ def main():
     end = start + len(all_copy_seq[shuffle_order[i]])
     annotation_fh.write("\t".join((genome_name, str(start), str(end), \
       all_copy_name[shuffle_order[i]], \
-      str(len(all_copy_mut_sites[shuffle_order[i]])), "+")) + "\n")
+      str(all_copy_mut_sites[shuffle_order[i]]), "+")) + "\n")
 
   annotation_fh.close()
 
   # output the full length sequences in FASTA for model training,
   # or output the inserted sequences
   outr = open(opt.out_repeats, 'w')
-  #for i in new_family.copies_fl:
   for i in xrange(len(all_copy_seq)):
     outr.write(">" + all_copy_name[i] + "\n")
     outr.write(text_wrap(all_copy_seq[i]))
